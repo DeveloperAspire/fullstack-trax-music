38edef659f6b113ad572a5b89f6ddc10c95d3ec7
DeveloperAspire <franklinokolie86@gmail.com>
2022-04-30 02:45:18 +0100

    Setup the next project, installed dependencies and added configurations, extended themes for Chakra UI

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,43 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+import "reset-css";
 
-export default MyApp
+const theme = extendTheme({
+  colors: {
+    gray: {
+      100: "#F5f5f5",
+      200: "#EEEEEE",
+      300: "#E0E0E0",
+      400: "BDBDBD",
+      500: "9E9E9E",
+      600: "757575",
+      700: "616161",
+      800: "#424242",
+      900: "#212121",
+    },
+
+    components: {
+      Button: {
+        variants: {
+          link: {
+            ":focus": {
+              outline: "none",
+              boxShadow: "none",
+            },
+          },
+        },
+      },
+    },
+  },
+});
+
+const MyApp = ({ Component, pageProps }: AppProps) => {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />;
+    </ChakraProvider>
+  );
+};
+
+export default MyApp;

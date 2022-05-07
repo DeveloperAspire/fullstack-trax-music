d2ef75f6e8a2781563e0577d2363ea355944b143
DeveloperAspire <franklinokolie86@gmail.com>
2022-05-07 01:30:21 +0100

    Upgraded the version of nextjs

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,17 +1,16 @@
 import { NextResponse } from "next/server";
 
-const signedinPages = ['/', '/playlist', "/library"];
-
+const signedinPages = ["/", "/playlist", "/library"];
 
 export default function middleware(req) {
-      if(signedinPages.find((p)=> p === req.nextUrl.pathname)) {
-            const token = req.cookies.TRAX_ACCESS_TOKEN
+  if (signedinPages.find((p) => p === req.nextUrl.pathname)) {
+    const token = req.cookies.TRAX_ACCESS_TOKEN;
 
-            const url = req.nextUrl.clone();
-            url.pathname = "/sign-in";
+    const url = req.nextUrl.clone();
+    url.pathname = "/sign-in";
 
-            if(!token) {
-                  return NextResponse.redirect(url)
-            }
-      }
+    if (!token) {
+      return NextResponse.redirect(url);
+    }
+  }
 }
